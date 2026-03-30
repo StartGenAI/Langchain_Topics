d3ca2e4afe6055b693d65b816f61c42dd7ee0956
Prashant6306 <parhad.prashant@gmail.com>
2026-03-30 22:59:18 +0530

    document loader2

--- 08.Document_Loader_30_March/01.text_loader.py
+++ 08.Document_Loader_30_March/01.text_loader.py
@@ -1,6 +1,6 @@
 from langchain_community.document_loaders import TextLoader
 
-file=TextLoader("/home/prashant/Desktop/Langchain_Topics/01.Langchain_Intro_and_models_13_March/doc1.txt",encoding="utf-8")
+file=TextLoader("doc1.txt",encoding="utf-8")
 
 document=file.load()
 
